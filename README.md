87a70363c33972174ee33593ecd6681e5687abe9
shrln <shrln0@proton.me>
2024-11-27 18:42:57 +0100

    Some cleaning

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
 # Blacklight
-
-## Using Blacklight with Deno
-
-```shell
-deno run dev
-```
-
-```shell
-deno run build
-```
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,3 @@
-import { generateJS } from "jsr:@helionaires/engram@0.2.6";
-
 export default function main() {
-    const bytecode = [
-        { opcode: 1, operand: 0b01 },
-        { opcode: 3, operand: 0b01 },
-        { opcode: 2, operand: 0b01 },
-    ];
-    const js = generateJS(bytecode, 1);
-    console.log(js);
-    console.log(toBinary(eval(js), 32));
-}
-
-function toBinary(n: bigint, padding = 0): string {
-    return n.toString(2).replace("-", "").padStart(padding, "0");
+    console.log("Hello World!");
 }
