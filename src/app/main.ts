86a3490e0018d1163dea2dda048b921de3e67863
shrln <shrln0@proton.me>
2024-11-27 17:09:58 +0100

    Update to Engram v2

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,3 +1,16 @@
+import { generateJS } from "jsr:@helionaires/engram@0.2.6";
+
 export default function main() {
-    console.log("Hello, world!");
+    const bytecode = [
+        { opcode: 1, operand: 0b01 },
+        { opcode: 3, operand: 0b01 },
+        { opcode: 2, operand: 0b01 },
+    ];
+    const js = generateJS(bytecode, 1);
+    console.log(js);
+    console.log(toBinary(eval(js), 32));
+}
+
+function toBinary(n: bigint, padding = 0): string {
+    return n.toString(2).replace("-", "").padStart(padding, "0");
 }
